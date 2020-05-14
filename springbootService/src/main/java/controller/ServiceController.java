2db63040e4085bb7ed609743b37787d251aa55a9
zhangbinxgit <1099528166@qq.com>
2020-05-14 17:05:00 +0800

    第一次提交

---
+++ springbootService/src/main/java/controller/ServiceController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import common.entity.RestfulResult;
+import common.entity.ServiceInfo;
+import common.utils.CommonUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController // 重要，如果用Controller会404
+@RequestMapping(value = "service")
+public class ServiceController {
+
+    @RequestMapping(value = "hello")
+    public void login(HttpServletRequest request, HttpServletResponse response,
+                      @RequestBody ServiceInfo serviceInfo) {
+
+        RestfulResult restfulResult = new RestfulResult();
+
+        try {
+            restfulResult.setData("Service1:Welcome " + serviceInfo.getName() + "!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        CommonUtils.printDataJason(response, restfulResult);
+    }
+
+    @RequestMapping(value = "rest")
+    public String rest(@RequestBody ServiceInfo serviceInfo) {
+
+        return "Service1:Welcome " + serviceInfo.getName() + " !";
+    }
+}
